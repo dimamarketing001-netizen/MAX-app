078698fb0ade550d6e7826c8974c4b55dbffa386
dimamarketing001-netizen <dimamarketing001@gmail.com>
2026-04-09 00:06:43 +0500

    RE

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5004, // Устанавливаем порт 5004 для dev-сервера
+  },
 })
